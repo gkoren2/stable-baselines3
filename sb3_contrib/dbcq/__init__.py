2b74906b76ae8e6472aa20388093dc0345dc18a4
gkoren2 <guy.koren@intel.com>
2021-02-02 15:42:09 +0200

    + adding skeletons for dbcq and dcql. need to fill in the algorithms

---
+++ sb3_contrib/dbcq/__init__.py
@@ -0,0 +1,5 @@
+# from sb3_contrib.qrdqn.policies import CnnPolicy as QRCnnPolicy
+# from sb3_contrib.qrdqn.policies import MlpPolicy as QRMlpPolicy
+from stable_baselines3.dqn.policies import CnnPolicy,MlpPolicy
+from sb3_contrib.dbcq.dbcq import DBCQ
+
